dd0c11b99804f8d05aad2a79a55a53809759e9f9
Mehri Russo <mehrimova@gmail.com>
2017-06-30 13:01:12 -0600

    started fresh, able to add items

--- app.js
+++ app.js
@@ -1,25 +1,105 @@
-"use strict";
-var app = angular.module('roi', []);
 
-app.controller('StartupController', function ($scope) {
-  var date = new Date().getDate();
 
-  $scope.funding = { dailycount:0, monthlycount:0, dailyaftertax:0, monthlyaftertax:0 };
 
-  var computeDaily = function() {
-    $scope.funding.dailyaftertax = (($scope.funding.dailycount * 28) / 1.33)
+// var CalcCtrl;
+
+// CalcCtrl = function($scope) {
+
+
+angular.module("calcApp", [])
+.filter('sumOfValue', function() {
+    return function(data, key) {
+      debugger;
+      if (angular.isUndefined(data) || angular.isUndefined(key))
+        return 0;
+      var sum = 0;
+
+      angular.forEach(data, function(v, k) {
+        sum = sum + parseInt(v[key]);
+      });
+      return sum;
+    }
+  }).controller("CalcCtrl", function($scope) {
+  $scope.items = [
+    {
+      text: "learn angular",
+      oneTime: 25,
+      monthly: 100,
+      done: false
+    }, {
+      text: "build an angular app",
+      oneTime: 35,
+      monthly: 100,
+      done: false
+    }
+  ];
+
+  $scope.addItem = function() {
+    $scope.items.push({
+      text: $scope.itemText,
+      oneTime: $scope.itemOne,
+      monthly: $scope.itemMonthly,
+      done: false
+    });
+
+    $scope.itemText = "";
+
+    return $scope.itemText;
   };
 
-  var computeMonthly = function() {
-    $scope.funding.monthlyaftertax = ((28 * $scope.funding.monthlycount / date) / 1.33 )
+  $scope.remaining = function() {
+    var count;
+    count = 0;
+
+    angular.forEach($scope.items, function(item) {
+      return count += item.done ? 0 : 1;
+    });
+
+    return count;
   };
 
-  $scope.$watch('funding.dailycount', computeDaily);
-  $scope.$watch('funding.monthlycount', computeMonthly);
+  $scope.archive = function() {
+    var oldItems;
+    oldItems = $scope.items;
+    $scope.items = [];
 
-});
+    angular.forEach(oldItems, function(item) {
+      if (!item.done) {
+        return $scope.items.push(item);
+      }
+    });
+  };
+
+//    $scope.revTotalOne = function() {
+//     var total = 0;
+
+//     angular.forEach($scope.items, function(item) {
+//       return item.oneTime + item.oneTime;
+//     });
+
+//     return total;
+//   };
 
 
-app.controller('TextController', function ($scope) {
-  $scope.text = "Welcome";
-});
+//    $scope.revTotalOne = function() {
+//     var total = 0;
+
+//     angular.forEach($scope.items, function(item) {
+//       return item.oneTime + item.oneTime;
+//     });
+
+//     return total;
+};
+
+
+
+
+
+};
+
+//take all item.oneTime and add together
+
+// var rev.Total = 0
+//rev.Total = revTotalOne + revTotalMonth
+
+// angular.module("calcApp", []).controller("CalcCtrl", CalcCtrl);
